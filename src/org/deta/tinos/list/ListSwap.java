934810d679c43932ebc35a4d0249d1ea21fcf048
Lenovo LYG <Lenovo@LAPTOP-352KOFCM 313699483>
2019-08-24 14:55:45 +0800

    更新了list的一些swap，我之后会把关于各种对象的排序更新。 20190824 罗瑶光

--- src/org/deta/tinos/list/ListSwap.java
+++ src/org/deta/tinos/list/ListSwap.java
@@ -1,4 +1,5 @@
 package org.deta.tinos.list;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -23,5 +24,19 @@ public class ListSwap{
 	public static String listToMap(Gson gson, List<Object> list){
 		return gson.fromJson(new JSONObject(gson.toJson(list)).toString()
 				, new TypeToken<Map<String, Object>>(){}.getType());
+	}
+
+	public static Object[] listToObjectArray(Gson gson, List<Object> list){
+		Object[] object= new Object[list.size()];
+		Iterator<Object> iterator= list.iterator();
+		int i=0;
+		while(iterator.hasNext()) {
+			object[i++]= iterator.next();
+		}
+		return object;
+	}
+
+	public static Iterator<Object> listToIterator(Gson gson, List<Object> list){
+		return list.iterator();
 	}
 }

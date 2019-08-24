5f7e38a884dbfc0f33d0c57817c9cef0c6cdc7f4
Lenovo LYG <Lenovo@LAPTOP-352KOFCM 313699483>
2019-08-25 01:50:34 +0800

    增加了Date数据包。 20190825 罗瑶光

--- src/org/deta/tinos/date/DateSwap.java
+++ src/org/deta/tinos/date/DateSwap.java
@@ -5,20 +5,25 @@ public class DateSwap{
 	public static String dateToGMTString(Date date) {	
 		return date.toGMTString();
 	}
-	
+
 	public static String dateToYYYYMMDD(Date date) {	
 		return ""+ date.getYear()+ ":"+ date.getMonth()+ ":"+ date.getDay();
 	}
-	
+
 	public static String dateToHHMMSS(Date date) {	
 		return ""+ date.getHours()+ ":"+ date.getMinutes()+ ":"+ date.getSeconds();
 	}
-	
+
 	public static String dateToMiliSeconds(Date date) {	
 		return ""+ date.getTime();
 	}
-	
+
 	public static String getCurrentMiliSeconds(Date date) {	
 		return ""+ new Date().getTime();
+	}
+
+	public static String dateToTimeStamp(Date date) {	
+		return ""+ date.getYear()+ "-"+ date.getMonth()+ "-"+ date.getDay()+ 
+				" "+ date.getHours()+ ":"+ date.getMinutes()+ ":"+ date.getSeconds();
 	}
 }

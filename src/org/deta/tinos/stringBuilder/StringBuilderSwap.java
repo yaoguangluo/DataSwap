a372244612739ddb2beb1ec09fc0b1177d71cec7
Lenovo LYG <Lenovo@LAPTOP-352KOFCM 313699483>
2019-08-25 01:07:03 +0800

    加了些String builder  罗瑶光 20190825

---
+++ src/org/deta/tinos/stringBuilder/StringBuilderSwap.java
@@ -0,0 +1,27 @@
+package org.deta.tinos.stringBuilder;
+import org.json.JSONObject;
+import org.json.XML;
+import com.google.gson.Gson;
+public class StringBuilderSwap{
+	public static char[] stringBuilderToCharArray(StringBuilder stringBuilder) {	
+		return stringBuilder.toString().toCharArray();
+	}	
+	
+	public static String stringBuilderToString(StringBuilder stringBuilder) {	
+		return stringBuilder.toString();
+	}	
+	
+	public static String stringBuilderToJson(Gson gson, StringBuilder stringBuilder) {	
+		return gson.toJson(stringBuilder.toString());
+	}	
+	
+	public static String stringBuilderToXml(Gson gson, StringBuilder stringBuilder){
+		JSONObject jSONObject= new JSONObject(gson.toJson(stringBuilder.toString()));
+		return XML.toString(jSONObject);	
+	}
+	
+	public static String[] stringBuilderToArray(String stopBy, StringBuilder stringBuilder){
+		String[] strings= stringBuilder.toString().split(stopBy); 
+		return strings;	
+	}
+}

d8a5bc06f422a3fb51614ca15de34df72d85fcbc
Lenovo LYG <Lenovo@LAPTOP-352KOFCM 313699483>
2019-08-25 23:23:11 +0800

    更新一些函数，持续修正。 20190825 罗瑶光

---
+++ src/org/deta/tinos/image/ImageSwap.java
@@ -0,0 +1,43 @@
+package org.deta.tinos.image;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+public class ImageSwap{
+	public static void pixTableToFile(String pngOutputPath, int[][] pix
+			, String fileType) throws IOException{
+		BufferedImage bufferedImage= new BufferedImage(pix[0].length
+				, pix.length, BufferedImage.TYPE_INT_RGB);
+		for (int i= 0; i< bufferedImage.getHeight(); ++i) {
+			for (int j= 0; j< bufferedImage.getWidth(); ++j) {
+				bufferedImage.setRGB(j, i, (pix[i][j]<< 16)
+						| (pix[i][j]<< 8)| (pix[i][j]));
+			}
+		}
+		ImageIO.write(bufferedImage, fileType, new File(pngOutputPath));	
+	}
+
+	public static void pixRGBTableToFile(String pngOutputPath, int[][] pixRed, int[][] pixGreen
+			, int[][] pixBlue, String fileType) throws IOException{
+		BufferedImage bufferedImage= new BufferedImage(pixRed[0].length, pixRed.length
+				, BufferedImage.TYPE_INT_RGB);
+		for (int i= 0; i< bufferedImage.getHeight(); ++i) {
+			for (int j= 0; j< bufferedImage.getWidth(); ++j) {
+				bufferedImage.setRGB(j, i, (pixRed[i][j]<< 16)
+						| (pixGreen[i][j]<< 8)| (pixBlue[i][j]));
+			}
+		}
+		ImageIO.write(bufferedImage, fileType, new File(pngOutputPath));	
+	}
+
+	public static void bufferedImageToFile(String pngOutputPath, BufferedImage bufferedImage
+			, String fileType) throws IOException{
+		ImageIO.write(bufferedImage, fileType, new File(pngOutputPath));	
+	}
+
+	public static void ImageToFile(String pngOutputPath, Image image, String fileType)
+			throws IOException{
+		ImageIO.write((BufferedImage)image, fileType, new File(pngOutputPath));	
+	}	
+}
--- src/org/deta/tinos/pdfProcessor/PDFSwap.java
+++ src/org/deta/tinos/pdfProcessor/PDFSwap.java
@@ -1,3 +1,12 @@
 package org.deta.tinos.pdfProcessor;
+import java.util.List;
 public class PDFSwap{
+	public static void pdfToTxt() {
+		
+	}
+	
+	public static List<Object> pdfToList(String pdfPath) {
+		
+		return null;	
+	}
 }

d1de551d0b8e822cf7f523b2fd011c84fc3ee86a
Lenovo LYG <Lenovo@LAPTOP-352KOFCM 313699483>
2019-08-27 11:45:40 +0800

    增加两个我常用到的函数。 20190827

--- src/org/deta/tinos/map/MapSwap.java
+++ src/org/deta/tinos/map/MapSwap.java
@@ -1,5 +1,10 @@
 package org.deta.tinos.map;
 import org.json.XML;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import org.json.JSONObject;
 import com.google.gson.Gson;
@@ -7,10 +12,31 @@ public class MapSwap{
 	public static String mapToJsonString(Gson gson, Map<String, Object> map){
 		return gson.toJson(map);
 	}
-	
+
 	public static String mapToXml(Gson gson, Map<String, Object> map){
 		JSONObject jSONObject= new JSONObject(gson.toJson(map));
 		return XML.toString(jSONObject);	
 	}
-
+	
+	public static List<Map<String, Object>> mapToList(Map<String, Object> map){
+		List<Map<String, Object>> list= new ArrayList<>();
+		Iterator<String> iterator= map.keySet().iterator();
+		while(iterator.hasNext()) {
+			String string= iterator.next();
+			Map<String, Object> singer= new HashMap<>();
+			singer.put(string, map.get(string));
+			list.add(singer);
+		}
+		return list;	
+	}
+	
+	public static Hashtable<String, Object> mapToHash(Map<String, Object> map){
+		Hashtable<String, Object> hashtable= new Hashtable<>();
+		Iterator<String> iterator= map.keySet().iterator();
+		while(iterator.hasNext()) {
+			String string= iterator.next();
+			hashtable.put(string, map.get(string));
+		}
+		return hashtable;
+	}
 }

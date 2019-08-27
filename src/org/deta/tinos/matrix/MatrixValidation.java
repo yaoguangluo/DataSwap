e96e25441bd26f77f4c16ccddfd1bf574f7b7989
Lenovo LYG <Lenovo@LAPTOP-352KOFCM 313699483>
2019-08-28 05:53:34 +0800

    增加了矩阵的类型检查修正 20190828 罗瑶光

---
+++ src/org/deta/tinos/matrix/MatrixValidation.java
@@ -0,0 +1,154 @@
+package org.deta.tinos.matrix;
+public class MatrixValidation{
+	public static boolean matrixCheck(Object[][] objects, String attribute){
+		if(attribute.equalsIgnoreCase("boolean")) {
+			for(int i= 0; i< objects.length; i++) {
+				for(int j= 0; j< objects[0].length; j++) {
+					if(!(objects[i][j] instanceof Boolean)) {
+						return false;
+					}
+				}
+			}
+		}
+		if(attribute.equalsIgnoreCase("int")) {
+			for(int i= 0; i<objects.length; i++) {
+				for(int j= 0; j<objects[0].length; j++) {
+					if(!(objects[i][j] instanceof Integer)) {
+						return false;
+					}
+				}
+			}
+		}
+		if(attribute.equalsIgnoreCase("long")) {
+			for(int i=0;i<objects.length;i++) {
+				for(int j=0;j<objects[0].length;j++) {
+					if(!(objects[i][j] instanceof Long)) {
+						return false;
+					}
+				}
+			}
+		}
+		if(attribute.equalsIgnoreCase("double")) {
+			for(int i=0;i<objects.length;i++) {
+				for(int j=0;j<objects[0].length;j++) {
+					if(!(objects[i][j] instanceof Double)) {
+						return false;
+					}
+				}
+			}
+		}
+		if(attribute.equalsIgnoreCase("float")) {
+			for(int i=0;i<objects.length;i++) {
+				for(int j=0;j<objects[0].length;j++) {
+					if(!(objects[i][j] instanceof Float)) {
+						return false;
+					}
+				}
+			}
+		}
+		if(attribute.equalsIgnoreCase("string")) {
+			for(int i=0;i<objects.length;i++) {
+				for(int j=0;j<objects[0].length;j++) {
+					if(!(objects[i][j] instanceof String)) {
+						return false;
+					}
+				}
+			}
+		}
+		if(attribute.equalsIgnoreCase("short")) {
+			for(int i=0;i<objects.length;i++) {
+				for(int j=0;j<objects[0].length;j++) {
+					if(!(objects[i][j] instanceof Short)) {
+						return false;
+					}
+				}
+			}
+		}
+		if(attribute.equalsIgnoreCase("byte")) {
+			for(int i=0;i<objects.length;i++) {
+				for(int j=0;j<objects[0].length;j++) {
+					if(!(objects[i][j] instanceof Byte)) {
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+	}
+
+	public static Object[][] matrixFix(Object[][] objects, String attribute){
+		if(attribute.equalsIgnoreCase("boolean")) {
+			for(int i= 0; i< objects.length; i++) {
+				for(int j= 0; j< objects[0].length; j++) {
+					if(!(objects[i][j] instanceof Boolean)) {
+						objects[i][j]= false;
+					}
+				}
+			}
+		}
+		if(attribute.equalsIgnoreCase("int")) {
+			for(int i= 0; i<objects.length; i++) {
+				for(int j= 0; j<objects[0].length; j++) {
+					if(!(objects[i][j] instanceof Integer)) {
+						objects[i][j]= 0;
+					}
+				}
+			}
+		}
+		if(attribute.equalsIgnoreCase("long")) {
+			for(int i=0;i<objects.length;i++) {
+				for(int j=0;j<objects[0].length;j++) {
+					if(!(objects[i][j] instanceof Long)) {
+						objects[i][j]= 0;
+					}
+				}
+			}
+		}
+		if(attribute.equalsIgnoreCase("double")) {
+			for(int i=0;i<objects.length;i++) {
+				for(int j=0;j<objects[0].length;j++) {
+					if(!(objects[i][j] instanceof Double)) {
+						objects[i][j]= 0.0;
+					}
+				}
+			}
+		}
+		if(attribute.equalsIgnoreCase("float")) {
+			for(int i=0;i<objects.length;i++) {
+				for(int j=0;j<objects[0].length;j++) {
+					if(!(objects[i][j] instanceof Float)) {
+						objects[i][j]= 0.0;
+					}
+				}
+			}
+		}
+		if(attribute.equalsIgnoreCase("string")) {
+			for(int i=0;i<objects.length;i++) {
+				for(int j=0;j<objects[0].length;j++) {
+					if(!(objects[i][j] instanceof String)) {
+						objects[i][j]= "";
+					}
+				}
+			}
+		}
+		if(attribute.equalsIgnoreCase("short")) {
+			for(int i=0;i<objects.length;i++) {
+				for(int j=0;j<objects[0].length;j++) {
+					if(!(objects[i][j] instanceof Short)) {
+						objects[i][j]= 0;
+					}
+				}
+			}
+		}
+		if(attribute.equalsIgnoreCase("byte")) {
+			for(int i=0;i<objects.length;i++) {
+				for(int j=0;j<objects[0].length;j++) {
+					if(!(objects[i][j] instanceof Byte)) {
+						objects[i][j]= 0;
+					}
+				}
+			}
+		}
+		return objects;
+	}
+}

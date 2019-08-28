f981cbd2cbf72737f71058937e1afa12fafe6da3
Lenovo LYG <Lenovo@LAPTOP-352KOFCM 313699483>
2019-08-29 01:33:46 +0800

    fix 20190829 罗瑶光

--- src/org/deta/tinos/string/StringValidation.java
+++ src/org/deta/tinos/string/StringValidation.java
@@ -2,8 +2,8 @@ package org.deta.tinos.string;
 
 public class StringValidation{
 	public static String stringCodeFilter(String string, String swapSymbol, String collection){
-		StringBuilder stringBuilder=new StringBuilder();
-		for(int i=0;i<string.length();i++) {
+		StringBuilder stringBuilder= new StringBuilder();
+		for(int i= 0; i< string.length(); i++) {
 			if(collection.contains(""+ string.charAt(i))){
 				stringBuilder.append(swapSymbol);
 			}else {

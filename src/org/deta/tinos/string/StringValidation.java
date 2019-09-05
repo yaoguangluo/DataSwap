3ede817b58cc597293982369545778942d65b3f0
Lenovo LYG <Lenovo@LAPTOP-352KOFCM 313699483>
2019-09-06 02:24:44 +0800

    增加了map的string过滤机制 20190906 罗瑶光

--- src/org/deta/tinos/string/StringValidation.java
+++ src/org/deta/tinos/string/StringValidation.java
@@ -1,4 +1,6 @@
 package org.deta.tinos.string;
+import java.util.Map;
+
 import org.deta.tinos.stable.Stable;
 public class StringValidation{
 	public static String stringCodeFilter(String string, String swapSymbol
@@ -6,6 +8,19 @@ public class StringValidation{
 		StringBuilder stringBuilder= new StringBuilder();
 		for(int i= 0; i< string.length(); i++) {
 			if(collection.contains(Stable.STRING_EMPTY+ string.charAt(i))){
+				stringBuilder.append(swapSymbol);
+			}else {
+				stringBuilder.append(string.charAt(i));
+			}
+		}
+		return stringBuilder.toString();
+	}
+	
+	public static String stringCodeFilter(String string, String swapSymbol
+			, Map<String, Boolean> collection){
+		StringBuilder stringBuilder= new StringBuilder();
+		for(int i= 0; i< string.length(); i++) {
+			if(collection.containsKey(Stable.STRING_EMPTY+ string.charAt(i))){
 				stringBuilder.append(swapSymbol);
 			}else {
 				stringBuilder.append(string.charAt(i));

78acfd02fa7dfc0d50f5aa8b6e98f46ed61c73a3
Lenovo LYG <Lenovo@LAPTOP-352KOFCM 313699483>
2019-09-06 04:04:28 +0800

    修正字符串大小写 带精度排序 20190906 罗瑶光

--- src/org/deta/tinos/string/StringSequency.java
+++ src/org/deta/tinos/string/StringSequency.java
@@ -1,13 +1,11 @@
 package org.deta.tinos.string;
-
-import java.io.UnsupportedEncodingException;
-
 public class StringSequency{
 	public static String[] stringToSequency(String[] string, int scale){
 		//比较头
 		for(int i= 0; i< string.length; i++) {
 			for(int j= 0; j< string.length; j++) {
-				if(string[i].charAt(0)< string[j].charAt(0)) {
+				if(string[i].toLowerCase().charAt(0)
+						< string[j].toLowerCase().charAt(0)) {
 					String temp= string[i].toString();
 					string[i]= string[j].toString();
 					string[j]= temp.toString();
@@ -15,14 +13,16 @@ public class StringSequency{
 			}
 		}	
 		//比较身
-		for(int k= 1; k< scale;k++) {
+		for(int k= 1; k< scale; k++) {
 			for(int i= 0; i< string.length; i++) {
 				for(int j= 0; j< string.length; j++) {
 					if(string[i].length()> k&& string[j].length()> k) {
-						if(string[i].charAt(k)< string[j].charAt(k)) {
+						if(string[i].toLowerCase().charAt(k)
+								< string[j].toLowerCase().charAt(k)) {
 							boolean find= true;
 							for(int p= 0; p< k; p++) {
-								if(string[i].charAt(p)!= string[j].charAt(p)) {
+								if(string[i].toLowerCase().charAt(p)
+										!= string[j].toLowerCase().charAt(p)) {
 									find= false;
 								}
 							}
@@ -38,17 +38,17 @@ public class StringSequency{
 		}
 		return string;
 	}
-		//DEMO
-		public static void main(String[] argv) throws UnsupportedEncodingException {
-			String testValue[]= new String[4];
-			testValue[0]="bacded";
-			testValue[2]="abcd";
-			testValue[1]="abcedd";
-			testValue[3]="bcaef";
-			testValue= stringToSequency(testValue, 6);	
-			System.out.println(testValue[0]);
-			System.out.println(testValue[1]);
-			System.out.println(testValue[2]);
-			System.out.println(testValue[3]);	
-		}
+	//		//DEMO
+	//		public static void main(String[] argv) throws UnsupportedEncodingException {
+	//			String testValue[]= new String[4];
+	//			testValue[0]= "bacded";
+	//			testValue[2]= "Abcd";
+	//			testValue[1]= "aBcedd";
+	//			testValue[3]= "bCaef";
+	//			testValue= stringToSequency(testValue, 6);	
+	//			System.out.println(testValue[0]);
+	//			System.out.println(testValue[1]);
+	//			System.out.println(testValue[2]);
+	//			System.out.println(testValue[3]);	
+	//		}
 }

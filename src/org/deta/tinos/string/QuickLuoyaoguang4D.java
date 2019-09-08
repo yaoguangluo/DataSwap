9d4fd6def2df7ff5f263e01095e7d3ab2dc53bdd
Lenovo LYG <Lenovo@LAPTOP-352KOFCM 313699483>
2019-09-09 04:15:09 +0800

    首字母的小高峰过滤混合快排4 已经完善了, 准备开始优化。20190909 罗瑶光

--- src/org/deta/tinos/string/QuickLuoyaoguang4D.java
+++ src/org/deta/tinos/string/QuickLuoyaoguang4D.java
@@ -1,47 +1,77 @@
 package org.deta.tinos.string;
+import java.util.HashMap;
+import java.util.Map;
 public class QuickLuoyaoguang4D{
+	Map<String, Boolean> find= new HashMap<>();
 	public void quick4DStringArray(String[] a, int lp, int rp, int scale) {
+		quick4DString(a, lp, rp, 0);
+		for(int k= 1; k< scale; k++) {
+			quick4DString(a, lp, rp, k);
+		}
+	}
+
+	public void quick4DString(String[] a, int lp, int rp, int scale) {
 		if(lp< rp){
-			int c= rp- lp; if(c< 7){ int j;
-			for(int i= 1+ lp; i<= lp+ c; i++){
-				j= i; while(j>= 1+ lp){
-					if((a[j].toLowerCase().charAt(0))
-							< a[j- 1].toLowerCase().charAt(0)){
-						String temp= a[j].toString();
-						a[j]= a[j- 1].toString();
-						a[j- 1]= temp.toString();
-					}
-					j--;
-				}
-			}	
-			return;
-			}
 			int pos= partitionString(a, lp, rp, scale);
-			quick4DStringArray(a, lp, pos- 1, scale);
-			quick4DStringArray(a, pos+ 1, rp, scale);
+				quick4DString(a, lp, pos- 1, scale);
+				quick4DString(a, pos+ 1, rp, scale);
 		}
 	}
 
 	private int partitionString(String[] a, int lp, int rp, int scale) {
 		String x= a[lp].toLowerCase().charAt(scale)
-				< a[rp].toLowerCase().charAt(scale)? a[lp].toString(): a[rp].toString(); 
-				int lp1= lp;
-				while(lp1< rp){
-					while(!(a[lp1].toLowerCase().charAt(scale)
-							> x.toLowerCase().charAt(scale)&& rp< lp1)){
-						lp1++;
-					} 
-					while(a[rp].toLowerCase().charAt(scale)
-							> x.toLowerCase().charAt(scale)){
-						rp--;
-					}
-					if(lp1< rp){
-						String temp= a[rp].toString();
-						a[rp]= a[lp1].toString();
-						a[lp1]= temp.toString();
+				< a[rp].toLowerCase().charAt(scale)? a[lp]: a[rp]; 
+		int lp1= lp;
+		while(lp1< rp){
+			while(innerConditionUp(a, scale, x, lp1, rp)){
+				lp1++;
+			} 
+			while(innerConditionDown(a, scale, x, rp)){
+				rp--;
+			}
+			if(lp1< rp){
+				boolean find= true;
+				for(int p= 0; p< scale; p++) {
+					if(a[rp].charAt(p)!= a[lp1].charAt(p)) {
+						find= false;
 					}
 				}
-				a[lp]= a[rp].toString(); a[rp]= x.toString();
-				return rp;
+				if(find) {
+					String temp= a[rp];
+					a[rp]= a[lp1];
+					a[lp1]= temp;
+				}
+			}
+		}
+		if(lp1<rp) {
+			a[lp]= a[rp]; a[rp]= x;
+		}
+		return rp;	
+	}
+	
+	private boolean innerConditionUp(String[] a, int scale, String x, int lp1, int rp) {
+		if(lp1>= a.length) {
+			return false;
+		}
+		if(a[lp1].length()<= scale|| x.length()<= scale) {
+			return true;	
+		}
+		if(a[lp1].toLowerCase().charAt(scale)<= x.toLowerCase().charAt(scale)&& lp1< rp) {
+			return true;
+		}
+		return false;	
+	}
+
+	private boolean innerConditionDown(String[] a, int scale, String x, int rp) {
+		if(rp>= a.length) {
+			return false;
+		}
+		if(a[rp].length()<= scale|| x.length()<= scale) {
+			return true;	
+		}
+		if(a[rp].toLowerCase().charAt(scale)> x.toLowerCase().charAt(scale)) {
+			return true;
+		}
+		return false;	
 	}
 }

59bb4baca3b92a47c9221fe4b1ce933ec49898c0
Lenovo LYG <Lenovo@LAPTOP-352KOFCM 313699483>
2019-09-15 06:25:03 +0800

    公式论证无效，已删除 20190915 罗瑶光

--- src/org/deta/tinos/fft/LBNFT.java
+++ src/org/deta/tinos/fft/LBNFT.java
@@ -1,54 +1,58 @@
-package org.deta.tinos.fft;
-public class LBNFT{
-	public final double Pi= 3.1415926535;
-	public final double size= 16;
-	public final double kPi= (Pi* 2)/ size;
-	public double[] swapDifferential(double[] input) {
-		double[] output= new double[(int)size];
-		for(int i= 0; i< size; i++) {
-			for(int j= 1; j< size; j+= (i+ 1)) {
-				double k= input[j]/input[j- 1];
-				output[j]+= k;
-			}
-		}
-		return output;	
-	}
-	public static void main(String[] argv) {
-		double[] input= new double[16];
-		input[0]=1;
-		input[1]=-30;
-		input[2]=-1000;
-		input[3]=30;
-		input[4]=1;
-		input[5]=30;
-		input[6]=1000;
-		input[7]=30;
-		input[8]=1;
-		input[9]=-30;
-		input[10]=-1000;
-		input[11]=30;
-		input[12]=1;
-		input[13]=30;
-		input[14]=1000;
-		input[15]=30;
-		double[] output= new LBNFT().swapDifferential(input);
-		input= new LBNFT().swapDifferential(output);
-		output= input;
-		System.out.println(output[0]);
-		System.out.println(output[1]);
-		System.out.println(output[2]);
-		System.out.println(output[3]);
-		System.out.println(output[4]);
-		System.out.println(output[5]);
-		System.out.println(output[6]);
-		System.out.println(output[7]);
-		System.out.println(output[8]);
-		System.out.println(output[9]);
-		System.out.println(output[10]);
-		System.out.println(output[11]);
-		System.out.println(output[12]);
-		System.out.println(output[13]);
-		System.out.println(output[14]);
-		System.out.println(output[15]);
-	}
-}
+//package org.deta.tinos.fft;
+//public class LBNFT{
+////公式无效，已删除；
+//	public static void main(String[] argv) {
+//		double[] input= new double[16];
+//		input[0]= 1;
+//		input[1]= 1;
+//		input[2]= 10000;
+//		input[3]= 1;
+//		input[4]= 1;
+//		input[5]= 1;
+//		input[6]= 1;
+//		input[7]= 1;
+//		input[8]= 1;
+//		input[9]= 1;
+//		input[10]=1;
+//		input[11]=1;
+//		input[12]=1;
+//		input[13]=1;
+//		input[14]=1;
+//		input[15]=1;
+//		double[] output= new LBNFT().ft(input);
+//		input= new LBNFT().ft(output);
+//		output= input;
+//		System.out.println(output[0]);
+//		System.out.println(output[1]);
+//		System.out.println(output[2]);
+//		System.out.println(output[3]);
+//		System.out.println(output[4]);
+//		System.out.println(output[5]);
+//		System.out.println(output[6]);
+//		System.out.println(output[7]);
+//		System.out.println(output[8]);
+//		System.out.println(output[9]);
+//		System.out.println(output[10]);
+//		System.out.println(output[11]);
+//		System.out.println(output[12]);
+//		System.out.println(output[13]);
+//		System.out.println(output[14]);
+//		System.out.println(output[15]);
+////		System.out.println(output[0]<0.000001?0:output[0]);
+////		System.out.println(output[1]<0.000001?0:output[1]);
+////		System.out.println(output[2]<0.000001?0:output[2]);
+////		System.out.println(output[3]<0.000001?0:output[3]);
+////		System.out.println(output[4]<0.000001?0:output[4]);
+////		System.out.println(output[5]<0.000001?0:output[5]);
+////		System.out.println(output[6]<0.000001?0:output[6]);
+////		System.out.println(output[7]<0.000001?0:output[7]);
+////		System.out.println(output[8]<0.000001?0:output[8]);
+////		System.out.println(output[9]<0.000001?0:output[9]);
+////		System.out.println(output[10]<0.000001?0:output[10]);
+////		System.out.println(output[11]<0.000001?0:output[11]);
+////		System.out.println(output[12]<0.000001?0:output[12]);
+////		System.out.println(output[13]<0.000001?0:output[13]);
+////		System.out.println(output[14]<0.000001?0:output[14]);
+////		System.out.println(output[15]<0.000001?0:output[15]);
+//	}
+//}

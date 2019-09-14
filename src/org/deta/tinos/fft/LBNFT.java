c16306db1cdad67fcaa9a7a1790df7a6531f1a5b
Lenovo LYG <Lenovo@LAPTOP-352KOFCM 313699483>
2019-09-15 01:34:45 +0800

    16位采样进行了可逆变换验证。 20190915 罗瑶光

--- src/org/deta/tinos/fft/LBNFT.java
+++ src/org/deta/tinos/fft/LBNFT.java
@@ -1,7 +1,7 @@
 package org.deta.tinos.fft;
 public class LBNFT{
 	public final double Pi= 3.1415926535;
-	public final double size= 8;
+	public final double size= 16;
 	public final double kPi= (Pi* 2)/ size;
 	public double[] swapDifferential(double[] input) {
 		double[] output= new double[(int)size];
@@ -14,16 +14,26 @@ public class LBNFT{
 		return output;	
 	}
 	public static void main(String[] argv) {
-		double[] input= new double[8];
+		double[] input= new double[16];
 		input[0]=1;
-		input[1]=30;
-		input[2]=1000;
+		input[1]=-30;
+		input[2]=-1000;
 		input[3]=30;
 		input[4]=1;
 		input[5]=30;
 		input[6]=1000;
 		input[7]=30;
+		input[8]=1;
+		input[9]=-30;
+		input[10]=-1000;
+		input[11]=30;
+		input[12]=1;
+		input[13]=30;
+		input[14]=1000;
+		input[15]=30;
 		double[] output= new LBNFT().swapDifferential(input);
+		input= new LBNFT().swapDifferential(output);
+		output= input;
 		System.out.println(output[0]);
 		System.out.println(output[1]);
 		System.out.println(output[2]);
@@ -32,5 +42,13 @@ public class LBNFT{
 		System.out.println(output[5]);
 		System.out.println(output[6]);
 		System.out.println(output[7]);
+		System.out.println(output[8]);
+		System.out.println(output[9]);
+		System.out.println(output[10]);
+		System.out.println(output[11]);
+		System.out.println(output[12]);
+		System.out.println(output[13]);
+		System.out.println(output[14]);
+		System.out.println(output[15]);
 	}
 }
